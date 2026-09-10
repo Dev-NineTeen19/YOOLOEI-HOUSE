a3448f6d2a6fe3c1c00d10b258c0439416e7e080
NineTeen1199 <teen423mo@gmail.com>
2026-09-10 12:05:24 +0700

    Setup_GitHub_Actions_deployment_and_SPA_routing

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: process.env.GITHUB_ACTIONS ? '/yooloei-house/' : './',
   server: {
     port: 5173,
     proxy: {
